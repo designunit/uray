44328144d07882df525e2139defcf7042629bc0d
Roman Timashev <roman@tmshv.ru>
2019-07-25 11:59:43 +0300

    dataset file chaned

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
         mapboxToken: 'pk.eyJ1IjoidG1zaHYiLCJhIjoiM3BMLVc2MCJ9.PM9ukwAm-YUGlrBqt4V6vw',
-        datasetUrl: 'https://www.dl.dropboxusercontent.com/s/j5j0iiyezuw0shl/20190719-oymyakon-vk.geojson?dl=0',
+        datasetUrl: 'https://www.dl.dropboxusercontent.com/s/nvd7d0b9jyp6roh/20190719-oymyakon-vk.geojson?dl=0',
     }
 }
