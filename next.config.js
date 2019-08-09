d6c777f83f7a898edc46cfdd1cf05241cb2e029b
Roman Timashev <roman@tmshv.ru>
2019-08-09 21:22:27 +0300

    next config update

--- next.config.js
+++ next.config.js
@@ -1,15 +1,11 @@
 const withCss = require('@zeit/next-css')
+const isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = withCss({
     env: {
-        GITHUB_TOKEN: '@github-token',
         API_BASE_URL: isProduction
             ? 'https://oymyakon.unit4.io/api'
             : 'http://localhost:8000',
-
-        GITHUB_OWNER: 'designunit',
-        GITHUB_REPO: 'oymyakon-data',
         MAPBOX_TOKEN: 'pk.eyJ1IjoidG1zaHYiLCJhIjoiM3BMLVc2MCJ9.PM9ukwAm-YUGlrBqt4V6vw',
-        DATASET_URL: 'https://www.dl.dropboxusercontent.com/s/nvd7d0b9jyp6roh/20190719-oymyakon-vk.geojson?dl=0',
-    }
+    },
 })
