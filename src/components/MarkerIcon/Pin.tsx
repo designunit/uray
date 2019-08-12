4ef1a1edbb0d07d7993e76425325f224f614d26e
Roman Timashev <roman@tmshv.ru>
2019-08-12 21:00:52 +0300

    Pin interface update

--- src/components/MarkerIcon/Pin.tsx
+++ src/components/MarkerIcon/Pin.tsx
@@ -5,10 +5,12 @@ const ICON = `M20.2,15.7L20.2,15.7c1.1-1.6,1.8-3.6,1.8-5.7c0-5.6-4.5-10-10-10S2,
   c0,0,0.1,0.1,0.1,0.2c0.2,0.3,0.4,0.6,0.7,0.9c2.6,3.1,7.4,7.6,7.4,7.6s4.8-4.5,7.4-7.5c0.2-0.3,0.5-0.6,0.7-0.9
   C20.1,15.8,20.2,15.8,20.2,15.7z`
 
-export const Pin: React.FC<IMarkerIconProps & {
-    size: number,
+export interface IPinProps {
+    size: number
     fill: string
-}> = props => (
+}
+
+export const Pin: React.FC<IMarkerIconProps & IPinProps> = props => (
     <svg
         height={props.size}
         viewBox={'0 0 24 24'}
