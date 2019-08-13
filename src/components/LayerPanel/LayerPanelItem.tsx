485971eb0621bb8216d052a29875d1d6b2929dcf
Roman Timashev <roman@tmshv.ru>
2019-08-13 14:32:51 +0300

    LayerPanel refactoing + design update

---
+++ src/components/LayerPanel/LayerPanelItem.tsx
@@ -0,0 +1,152 @@
+import * as React from 'react'
+import { List, Button, Switch, Icon, Input } from 'antd'
+import { Colorbox } from '../Colorbox'
+import { ILayer } from '../../app/types'
+
+export interface ILayerPanelItemProps {
+    style?: React.CSSProperties
+    onChangeVisible: (visible: boolean, index: number) => void
+    onClickLayerEdit: (layer: ILayer) => void
+    onDeleteLayer: (id: number) => Promise<void>
+    onAddLayer: () => Promise<void>
+    index: number // todo remove
+    item: {
+        id: number
+        name: string
+        color: string
+
+        info?: string
+        visible: boolean
+        readonly: boolean
+        render?: () => React.ReactNode
+    }
+}
+
+export const LayerPanelItem: React.FC<ILayerPanelItemProps> = props => {
+    const [isDeletingLayer, setDeletingLayer] = React.useState(false)
+    const [showExtraActions, setShowExtraActions] = React.useState(false)
+    const item = props.item
+
+    return (
+        <List.Item
+            onMouseOver={() => {
+                setShowExtraActions(true)
+            }}
+            onMouseOutCapture={() => {
+                setShowExtraActions(false)
+            }}
+        >
+            <div className={'list'}>
+                <style jsx>{`
+                    .list {
+                        width: 100%;
+                    }
+
+                    section {
+                        display: flex;
+                        justify-content: space-between;
+                    }
+
+                    .action-block {
+                        display: flex;
+                        align-items: center;
+                    }
+
+                    .actions {
+                        display: flex;
+                        align-items: center;
+
+                        transition: opacity 0.15s ease-in-out;
+                    }
+                `}</style>
+
+                <section>
+                    <span>
+                        <Colorbox
+                            width={10}
+                            height={10}
+                            color={item.color}
+                            style={{
+                                marginRight: 5,
+                            }}
+                        />
+
+                        <span style={{
+                            marginRight: 5,
+                        }}>{item.name}</span>
+
+                        {!item.info ? null : (
+                            <span style={{
+                                color: '#ccc',
+                                marginRight: 5,
+                            }}>{`(${item.info})`}</span>
+                        )}
+                    </span>
+
+                    <div className={'action-block'}>
+                        <div
+                            className={'actions'}
+                            style={{
+                                opacity: showExtraActions ? 1 : 0,
+                                marginRight: 5,
+                            }}
+                        >
+                            {item.readonly ? null : (
+                                <>
+                                    <Button
+                                        loading={isDeletingLayer}
+                                        disabled={isDeletingLayer}
+                                        icon={'delete'}
+                                        size={'small'}
+                                        type={'link'}
+                                        onClick={async () => {
+                                            setDeletingLayer(true)
+                                            await props.onDeleteLayer(item.id)
+                                            setDeletingLayer(false)
+                                        }}
+                                    />
+                                    <Button
+                                        loading={isDeletingLayer}
+                                        disabled={isDeletingLayer}
+                                        icon={'edit'}
+                                        size={'small'}
+                                        type={'link'}
+                                        onClick={() => {
+                                            props.onClickLayerEdit(item)
+                                        }}
+                                    />
+                                </>
+                            )}
+
+                            <Button
+                                disabled={true}
+                                icon={'download'}
+                                size={'small'}
+                                type={'link'}
+                            />
+                        </div>
+
+                        <div className={'actions'}>
+                            <Switch
+                                defaultChecked={item.visible}
+                                unCheckedChildren={(
+                                    <Icon type={'eye-invisible'} />
+                                )}
+                                checkedChildren={(
+                                    <Icon type={'eye'} />
+                                )}
+                                onChange={(checked) => {
+                                    props.onChangeVisible(checked, props.index)
+                                }}
+                            />
+                        </div>
+                    </div>
+                </section>
+
+                {!item.render ? null : (
+                    item.render()
+                )}
+            </div>
+        </List.Item>
+    )
+}
--- src/components/LayerPanel/index.tsx
+++ src/components/LayerPanel/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { List, Button, Switch, Icon, Input } from 'antd'
-import { Colorbox } from '../Colorbox'
 import { ILayer } from '../../app/types'
+import { LayerPanelItem } from './LayerPanelItem'
 
 export interface ILayerPanelProps {
     style?: React.CSSProperties
@@ -14,7 +14,7 @@ export interface ILayerPanelProps {
         name: string
         color: string
 
-        info ?: string
+        info?: string
         visible: boolean
         readonly: boolean
         render?: () => React.ReactNode
@@ -23,7 +23,6 @@ export interface ILayerPanelProps {
 
 export const LayerPanel: React.FC<ILayerPanelProps> = props => {
     const [isAddingLayer, setAddingLayer] = React.useState(false)
-    const [isDeletingLayer, setDeletingLayer] = React.useState(false)
 
     return (
         <List
@@ -34,7 +33,7 @@ export const LayerPanel: React.FC<ILayerPanelProps> = props => {
                 <Button
                     icon={'plus'}
                     loading={isAddingLayer}
-                    disabled={true || isAddingLayer}
+                    disabled={isAddingLayer}
                     onClick={async () => {
                         setAddingLayer(true)
                         await props.onAddLayer()
@@ -44,109 +43,16 @@ export const LayerPanel: React.FC<ILayerPanelProps> = props => {
             )}
             bordered
             dataSource={props.items}
-            renderItem={(item, index) => {
-                return (
-                    <List.Item>
-                        <div className={'list'}>
-                            <style jsx>{`
-                                .list {
-                                    width: 100%;
-                                }
-
-                                section {
-                                    display: flex;
-                                    justify-content: space-between;
-                                }
-
-                                .actions {
-                                    display: flex;
-                                    align-items: center;
-                                }
-                            `}</style>
-
-                            <section>
-                                <span>
-                                    <span style={{
-                                        marginRight: 5,
-                                    }}>{item.name}</span>
-
-                                    {!item.info ? null : (
-                                        <span style={{
-                                            color: '#ccc',
-                                            marginRight: 5,
-                                        }}>{`(${item.info})`}</span>
-                                    )}
-
-                                    <Colorbox
-                                        width={25}
-                                        height={8}
-                                        color={item.color}
-                                    />
-                                </span>
-
-                                <div className={'actions'}>
-                                    {item.readonly ? null : (
-                                        <>
-                                            <Button
-                                                style={{
-                                                    marginRight: 5,
-                                                }}
-                                                loading={isDeletingLayer}
-                                                disabled={isDeletingLayer}
-                                                icon={'delete'}
-                                                size={'small'}
-                                                onClick={async () => {
-                                                    setDeletingLayer(true)
-                                                    await props.onDeleteLayer(item.id)
-                                                    setDeletingLayer(false)
-                                                }}
-                                            />
-                                            <Button
-                                                style={{
-                                                    marginRight: 5,
-                                                }}
-                                                loading={isDeletingLayer}
-                                                disabled={isDeletingLayer}
-                                                icon={'edit'}
-                                                size={'small'}
-                                                onClick={() => {
-                                                    props.onClickLayerEdit(item)
-                                                }}
-                                            />
-                                        </>
-                                    )}
-
-                                    <Button
-                                        disabled={true}
-                                        style={{
-                                            marginRight: 5,
-                                        }}
-                                        icon={'download'}
-                                        size={'small'}
-                                        type={'default'}
-                                    />
-
-                                    <Switch
-                                        defaultChecked={item.visible}
-                                        unCheckedChildren={(
-                                            <Icon type={'eye-invisible'} />
-                                        )}
-                                        checkedChildren={(
-                                            <Icon type={'eye'} />
-                                        )}
-                                        onChange={(checked) => {
-                                            props.onChangeVisible(checked, index)
-                                        }}
-                                    />
-                                </div>
-                            </section>
-                            {!item.render ? null : (
-                                item.render()
-                            )}
-                        </div>
-                    </List.Item>
-                )
-            }}
+            renderItem={(item, index) => (
+                <LayerPanelItem
+                    item={item}
+                    index={index}
+                    onChangeVisible={props.onChangeVisible}
+                    onClickLayerEdit={props.onClickLayerEdit}
+                    onDeleteLayer={props.onDeleteLayer}
+                    onAddLayer={props.onAddLayer}
+                />
+            )}
         />
     )
 }
