ce5556f783ea11de63a96cad02c96ee7420e8c5d
Roman Timashev <roman@tmshv.ru>
2019-08-18 18:20:09 +0300

    default color set in dataviz changed to category10

--- pages/dataviz.tsx
+++ pages/dataviz.tsx
@@ -193,7 +193,7 @@ const Page: NextPage<IPageProps> = (props) => {
 
             <Sankey
                 data={data}
-                defaultColorSet={['#e8c1a0', '#f47560', '#f1e15b', '#e8a838', '#61cdbb', '#97e3d5']}
+                defaultColorSet={["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"]}
             />
         </div>
     )
