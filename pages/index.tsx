879683be68718a14c24b4fb930fb7b1126200886
Roman Timashev <roman@tmshv.ru>
2019-08-18 17:40:04 +0300

    added osm style sample

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,6 +18,35 @@ const DynamicApp = dynamic(() => import('../src/components/App'), {
     ssr: false
 })
 
+function createOsmStyle() {
+    return {
+        style: {
+            "version": 8,
+            "sources": {
+                "osm-tiles": {
+                    "type": "raster",
+                    "tiles": [
+                        "http://a.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                        "http://b.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                        "http://c.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                    ],
+                    "tileSize": 256,
+                },
+            },
+            "layers": [
+                {
+                    "id": "osm-tiles",
+                    "type": "raster",
+                    "source": "osm-tiles",
+                    "minzoom": 0,
+                    "maxzoom": 22
+                },
+            ]
+        }
+    }
+}
+
+
 const MAP_STYLE_SATELLITE = 'satellite'
 const MAP_STYLE_VECTOR = 'vector'
 
