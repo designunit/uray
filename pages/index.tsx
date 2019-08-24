9a7c73202be1b010d85c2a5da029ffbd28efb94a
Roman Timashev <roman@tmshv.ru>
2019-08-24 15:10:34 +0300

    default map coord/zoom changed to uray

--- pages/index.tsx
+++ pages/index.tsx
@@ -146,8 +146,8 @@ const Page: NextPage<IPageProps> = (props) => {
                                         mapStyleOption={mapStyleOption}
                                         mapStyleOptions={mapStyleOptions}
                                         onChangeMapStyleOption={setMapStyleOption}
-                                        center={[63.46255030526142, 142.78664300880652]}
-                                        zoom={12}
+                                        center={[60.12380893107247, 64.79488837184576]}
+                                        zoom={13}
                                     />
                                 )
                         }}
