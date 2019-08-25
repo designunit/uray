4e9c8dd11c199d42920def2715b8616f539f5559
Roman Timashev <roman@tmshv.ru>
2019-08-26 00:43:09 +0300

    api url changes

--- next.config.js
+++ next.config.js
@@ -7,8 +7,8 @@ module.exports = withCss({
             ? 'wss://uray.tmshv.com/api/'
             : 'ws://tmshv.local:8000',
         API_BASE_URL: isProduction
-            ? 'https://oymyakon.unit4.io/api'
-            : 'http://localhost:8000',
+            ? 'https://uray.tmshv.com/api'
+            : 'http://tmshv.local:8000',
         MAPBOX_TOKEN: 'pk.eyJ1IjoidG1zaHYiLCJhIjoiM3BMLVc2MCJ9.PM9ukwAm-YUGlrBqt4V6vw',
         APP_ACCESS_MODE: 'readonly',
     },
