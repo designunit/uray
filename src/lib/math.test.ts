e4e0e868ce35ed7f9606167a912032576e85186a
Roman Timashev <roman@tmshv.ru>
2019-08-27 22:59:07 +0500

    added lib math constrain

--- src/lib/math.test.ts
+++ src/lib/math.test.ts
@@ -1,4 +1,4 @@
-import { round } from './math'
+import { round, constrain } from './math'
 
 describe('lib math', () => {
     describe('round', () => {
@@ -6,6 +6,26 @@ describe('lib math', () => {
             const sample = round(147.71239410235, 1000)
 
             expect(sample).toEqual(147.712)
+        })
+    })
+    
+    describe('constrain', () => {
+        it('should get value in range', () => {
+            const sample = constrain(17.4575637, 10, 20)
+
+            expect(sample).toEqual(17.4575637)
+        })
+        
+        it('should get min value in range if value is less than min', () => {
+            const sample = constrain(1.4575637, 5, 20)
+
+            expect(sample).toEqual(5)
+        })
+       
+        it('should get max value in range if value is more than max', () => {
+            const sample = constrain(17.4575637, 5, 15)
+
+            expect(sample).toEqual(15)
         })
     })
 })
--- src/lib/math.ts
+++ src/lib/math.ts
@@ -1,3 +1,13 @@
 export function round(value: number, n: number): number {
     return Math.round(value * n) / n
 }
+
+export function constrain(value: number, min: number, max: number): number {
+    if (value < min) {
+        return min
+    } else if (value > max) {
+        return max
+    } else {
+        return value
+    }
+}
