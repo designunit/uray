1b5b36a5e0e5e4c49447ffcb97b2ec1ea78cfac8
Roman Timashev <roman@tmshv.ru>
2019-08-29 07:59:51 +0500

    added lib time isValidDate

---
+++ src/lib/time.test.ts
@@ -0,0 +1,25 @@
+import { isValidDate } from './time'
+
+describe('lib time', () => {
+    describe('isValidDate', () => {
+        it('should properly match valid date string', () => {
+            expect(isValidDate('2019-08-29T02:14:16.880Z')).toBeTruthy()
+        })
+        
+        it('should properly match invalid any non date string', () => {
+            expect(isValidDate('wtf')).toBeFalsy()
+        })
+
+        it('should properly match invalid any non string objects', () => {
+            expect(isValidDate({})).toBeFalsy()
+            expect(isValidDate(true)).toBeFalsy()
+            expect(isValidDate([])).toBeFalsy()
+            expect(isValidDate(() => {})).toBeFalsy()
+        })
+
+        it('should properly match invalid null/undefined', () => {
+            expect(isValidDate(null)).toBeFalsy()
+            expect(isValidDate(undefined)).toBeFalsy()
+        })
+    })
+})
--- src/lib/time.ts
+++ src/lib/time.ts
@@ -1,5 +1,18 @@
+import { isNullOrUndefined } from 'util'
+
 export async function sleep(ms): Promise<void> {
     return new Promise(resolve => {
         setTimeout(resolve, ms)
     })
+}
+
+export function isValidDate(value: any): boolean {
+    if (typeof value === 'boolean') {
+        return false
+    } else if (isNullOrUndefined(value)) {
+        return false
+    }
+
+    const date = new Date(value)
+    return !isNaN(date.getDate())
 }
