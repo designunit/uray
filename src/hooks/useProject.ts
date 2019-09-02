d6792fad8036753f779d81fdd86c07d3449de9d1
Roman Timashev <roman@tmshv.ru>
2019-09-02 19:46:48 +0300

    useProject optimisation: update project with api only when needed

--- src/hooks/useProject.ts
+++ src/hooks/useProject.ts
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import equal from 'fast-deep-equal'
 import { IProjectDefinition } from '../app/types'
 import { projectReducer } from '../reducers/projectReducer'
 import { updateProject } from '../app/api'
@@ -6,11 +7,17 @@ import { updateProject } from '../app/api'
 export function useProject(initialState: IProjectDefinition): [IProjectDefinition, React.Dispatch<any>, boolean] {
     const [project, dispatch] = React.useReducer<React.Reducer<IProjectDefinition, any>>(projectReducer, initialState)
     const [updating, setUpdating] = React.useState(false)
+    const projectRef = React.useRef(initialState)
 
     React.useEffect(() => {
+        if (equal(projectRef.current, project)) {
+            return
+        }
+
         setUpdating(true)
         updateProject(project)
-            .then(() => {
+            .then((newProject) => {
+                projectRef.current = newProject
                 setUpdating(false)
             })
             .catch(() => {
