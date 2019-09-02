790b1df9466b62b555873265bff3c5bd817b9df4
Roman Timashev <roman@tmshv.ru>
2019-09-02 17:14:20 +0300

    layerIndexReducer removed unnecessary if

--- src/components/App/layerIndexReducer.ts
+++ src/components/App/layerIndexReducer.ts
@@ -14,14 +14,6 @@ export function layerIndexReducer(state: IIndex<ILayer>, action: any): IIndex<IL
         }
     }
 
-    if (action.type === ACTION_LAYER_SET) {
-        const layer = action.payload as ILayer
-        return {
-            ...state,
-            [layer.id]: layer,
-        }
-    }
-
     if (action.type === ACTION_LAYER_DELETE) {
         return omit(state, `${action.payload.id}`)
     }
