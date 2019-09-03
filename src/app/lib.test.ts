271af3228cffdc8143d2714a9f53b20ccce53c03
Roman Timashev <roman@tmshv.ru>
2019-09-03 20:54:20 +0300

    added app lib tests

---
+++ src/app/lib.test.ts
@@ -0,0 +1,11 @@
+import { treeKey } from './lib'
+
+describe('app lib', () => {
+    describe('treeKey', () => {
+        it('should create key based on input values', () => {
+            expect(treeKey('prefix', 'value')).toEqual(
+                'prefix-value'
+            )
+        })
+    })
+})
