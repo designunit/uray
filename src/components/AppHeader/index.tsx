ca14e5a6cabab3fcc0d435984bf816874d5210a6
Roman Timashev <roman@tmshv.ru>
2019-09-04 14:17:45 +0300

    AppHeader meta viewport tag added

--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -13,6 +13,7 @@ export interface IAppHeaderProps {
 export const AppHeader: React.FC<IAppHeaderProps> = props => (
     <>
         <Head>
+            <meta name='viewport' content='width=device-width, initial-scale=1.0' />
             <title>{props.title}</title>
         </Head>
 
