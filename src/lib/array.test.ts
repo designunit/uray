778d177f421c6de5c3a241689998607c3970b71f
Roman Timashev <roman@tmshv.ru>
2019-09-16 21:57:31 +0300

    array tests tslinted

--- src/lib/array.test.ts
+++ src/lib/array.test.ts
@@ -5,26 +5,26 @@ describe('lib array', () => {
         it('should properly swap item with next item', () => {
             const sample = ['q', 'w', 'e', 'r', 't', 'y']
             const result = moveItemByIndex(sample, 2, 1)
-            
+
             expect(result).toEqual(
-                ['q', 'w', 'r', 'e', 't', 'y']
+                ['q', 'w', 'r', 'e', 't', 'y'],
             )
         })
 
         it('should properly swap item with prev item', () => {
             const sample = ['q', 'w', 'e', 'r', 't', 'y']
             const result = moveItemByIndex(sample, 2, -1)
-            
+
             expect(result).toEqual(
-                ['q', 'e', 'w', 'r', 't', 'y']
+                ['q', 'e', 'w', 'r', 't', 'y'],
             )
         })
     })
-    
+
     describe('moveItemByIndex', () => {
         it('should properly map items to domains', () => {
             const sample = arrayToDomains(['q', 'w', 'e', 'r', 't', 'y'])
-            
+
             expect(sample).toEqual([
                 ['q', 'w'],
                 ['w', 'e'],
@@ -55,7 +55,7 @@ describe('lib array', () => {
         it('should return true if all items is true', () => {
             expect(all([true, true, true, true, true])).toBeTruthy()
         })
-        
+
         it('should return false if any item is false', () => {
             expect(all([true, true, false, true, true])).toBeFalsy()
         })
