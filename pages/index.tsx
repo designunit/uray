301e8fb3365b4f8c58ebe7bd133bb954e88ed52d
Roman Timashev <roman@tmshv.ru>
2019-09-27 16:30:41 +0300

    revert missing readonly variable

--- pages/index.tsx
+++ pages/index.tsx
@@ -64,6 +64,7 @@ const getMapStyle = (dark: boolean) => dark
 const loadProject = () => getProject(1)
 
 const Page: NextPage = () => {
+    const readonly = true // process.env.APP_ACCESS_MODE === 'readonly'
     const mapboxToken = process.env.MAPBOX_TOKEN || ''
     const wesocketUrl = process.env.API_WS_URL
 
