66d478afa4b1caff15eab97eec4c65201620af07
Roman Timashev <roman@tmshv.ru>
2019-09-30 20:52:26 +0300

    ImageMarker dont show until image loaded

--- src/infographics/components/PhotoMap/ImageMarker.tsx
+++ src/infographics/components/PhotoMap/ImageMarker.tsx
@@ -5,14 +5,21 @@ interface IImageMarkerProps {
     src: string
 }
 
-export const ImageMarker: React.FC<IImageMarkerProps> = React.memo(props => (
-    <div
-        style={{
-            width: `${props.size}px`,
-            height: `${props.size}px`,
-        }}
-    >
-        <style jsx>{`
+export const ImageMarker: React.FC<IImageMarkerProps> = React.memo(props => {
+    const [loaded, setLoaded] = React.useState(false)
+    const onLoad = React.useCallback(() => {
+        setLoaded(true)
+    }, [])
+
+    return (
+        <div
+            style={{
+                width: `${props.size}px`,
+                height: `${props.size}px`,
+                opacity: loaded ? null : 0,
+            }}
+        >
+            <style jsx>{`
                 div {
                     width: 50px;
                     height: 50px;
@@ -30,8 +37,10 @@ export const ImageMarker: React.FC<IImageMarkerProps> = React.memo(props => (
                     border-radius: 50%;
                 }
             `}</style>
-        <img
-            src={props.src}
-        />
-    </div>
-))
+            <img
+                src={props.src}
+                onLoad={onLoad}
+            />
+        </div>
+    )
+})
