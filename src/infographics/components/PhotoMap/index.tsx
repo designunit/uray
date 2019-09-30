0ca72c312dda70c06efdffe3b43487790ebaf521
Roman Timashev <roman@tmshv.ru>
2019-09-30 21:25:46 +0300

    PhotoMap component refactorint + clustering

--- src/infographics/components/PhotoMap/index.tsx
+++ src/infographics/components/PhotoMap/index.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react'
 
-import { Point } from 'geojson'
-import MapGL, { Marker, ViewState } from 'react-map-gl'
+import { Feature, Point } from 'geojson'
+import { Marker, ViewState } from 'react-map-gl'
 
-import { Pin } from '../../../components/MarkerIcon/Pin'
+import { Map } from 'mapbox-gl'
+import { Cluster } from '../../../components/Cluster'
+import { MapboxGL } from '../../../components/MapboxGL'
 import { ImageMarker } from './ImageMarker'
 import { useFeatures } from './useFeatures'
 
@@ -27,6 +29,7 @@ interface IPhotoMapProps {
 
 const PhotoMap: React.FC<IPhotoMapProps> = props => {
     const features = useFeatures<Point, IProperties>(props.dataUrl)
+    const mapRef = React.useRef<Map>()
     const [viewport, setViewport] = React.useState<ViewState>({
         latitude: props.startCoord.latitude,
         longitude: props.startCoord.longitude,
@@ -37,18 +40,30 @@ const PhotoMap: React.FC<IPhotoMapProps> = props => {
     const onChangeViewport = React.useCallback((newViewport: ViewState) => {
         setViewport(newViewport)
     }, [])
+    const onLoad = React.useCallback((map: Map) => {
+        mapRef.current = map
+    }, [])
+    const onClick = React.useCallback(() => {
+        // tslint:disable-next-line:no-console
+        console.log('click')
+    }, [])
 
     return (
-        <MapGL
-            {...viewport}
+        <MapboxGL
+            viewport={viewport}
+            zoom={props.startZoom}
+            onLoad={onLoad}
+            onClick={onClick}
+            center={[
+                props.startCoord.latitude,
+                props.startCoord.longitude,
+            ]}
             {...props.extra}
-            width={'100%'}
-            height={'100%'}
-            mapStyle={props.mapStyle}
-            mapboxApiAccessToken={props.mapboxToken}
-            onViewportChange={onChangeViewport}
+            mapStyle={props.mapStyle as string}
+            mapboxToken={props.mapboxToken}
+            onChangeViewport={onChangeViewport}
         >
-            {features.map(f => (
+            {/* {features.map(f => (
                 <Marker
                     key={f.id}
                     longitude={f.geometry.coordinates[0]}
@@ -59,8 +74,64 @@ const PhotoMap: React.FC<IPhotoMapProps> = props => {
                         src={f.properties.url}
                     />
                 </Marker>
-            ))}
-        </MapGL>
+            ))} */}
+
+            {!mapRef.current ? null : (
+                <Cluster
+                    map={mapRef.current}
+                    minZoom={0}
+                    maxZoom={22}
+                    radius={50}
+                    extent={512}
+                    nodeSize={64}
+                    data={features}
+                    // getDataHash={() => favIds.join('')}
+                    getDataHash={() => ''}
+                    clusterMap={(props: any) => ({
+                        url: props.url,
+                    })}
+                    clusterReduce={(acc, props: any) => ({
+                        urls: props.url,
+                    })}
+                    renderCluster={(cluster) => {
+                        const clusterId = cluster.properties.cluster_id
+                        const [longitude, latitude] = cluster.geometry.coordinates
+                        const clusterSize = cluster.properties.point_count
+
+                        const url = cluster.properties.url
+                        const size = Math.min(
+                            props.size + clusterSize,
+                            100,
+                        )
+
+                        return (
+                            <Marker
+                                key={`cluster-${clusterId}`}
+                                longitude={longitude}
+                                latitude={latitude}
+                            >
+                                <ImageMarker
+                                    size={size}
+                                    src={url}
+                                />
+                            </Marker>
+                        )
+                    }}
+                    renderFeature={(f: Feature<Point, IProperties>) => (
+                        <Marker
+                            key={f.id}
+                            longitude={f.geometry.coordinates[0]}
+                            latitude={f.geometry.coordinates[1]}
+                        >
+                            <ImageMarker
+                                size={props.size}
+                                src={f.properties.url}
+                            />
+                        </Marker>
+                    )}
+                />
+            )}
+        </MapboxGL>
     )
 }
 
